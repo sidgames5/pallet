ddf5c0fa7a80412898dedd3c18216fcd62325670
SidGames5 <saytleader@outlook.com>
2025-01-15 17:24:49 -0500

    fixed css

--- src/pages/explorer/Explorer.jsx
+++ src/pages/explorer/Explorer.jsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Database from "../../utils/Database";
 import { Link } from "react-router-dom";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import "./explorer.scss";
 
 function Explorer() {
     return (
--- src/pages/explorer/Layout.jsx
+++ src/pages/explorer/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet, NavLink } from "react-router-dom";
+import "./explorer.scss";
 
 const Layout = () => {
     return (
