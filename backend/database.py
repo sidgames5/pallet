27b654573f685012333c45d5570c1758516c3e7e
SidGames5 <saytleader@outlook.com>
2025-01-16 16:38:08 -0500

    better database handling

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,24 @@
 import json
 import os
 
+
 class Database:
+    data = {}
+
     @staticmethod
     def init():
         if os.path.exists(".run/main.db"):
+            with open(".run/main.db", "r", encoding="utf-8") as f:
+                Database.data = json.loads(f.read())
             return
         os.mkdir(".run")
+        Database.data = {"buildings": [], "areas": [], "shelves": [], "items": []}
         with open(".run/main.db", "w", encoding="utf-8") as f:
-            f.write(json.dumps({
-                "buildings": [],
-                "areas": [],
-                "shelves": [],
-                "items": []
-            }))
+            f.write(json.dumps(Database.data))
+            f.close()
+
+    @staticmethod
+    def save():
+        with open(".run/main.db", "w", encoding="utf-8") as f:
+            f.write(json.dumps(Database.data))
             f.close()
