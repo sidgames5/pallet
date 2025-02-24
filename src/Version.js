b66dda4d612dd9cc32e7a3ede4e8ee6cd157037b
SidGames5 <saytleader@outlook.com>
2025-02-24 15:44:10 -0500

    updated version

--- src/Version.js
+++ src/Version.js
@@ -2,7 +2,7 @@ class Version {
     static getMajor() { return 1; }
     static getMinor() { return 0; }
     static getPatch() { return 0; }
-    static getModifier() { return "dev"; }
+    static getModifier() { return "stable"; }
     static getVersion() {
         return `${Version.getMajor()}.${Version.getMinor()}.${Version.getPatch()}-${Version.getModifier}`;
     }
