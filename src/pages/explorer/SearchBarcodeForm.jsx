3229d197e955a6c4acb571a16398d0b32a82eb6a
Sid <saytleader@outlook.com>
2025-02-27 08:19:16 -0500

    made the error popup better

--- src/pages/explorer/SearchBarcodeForm.jsx
+++ src/pages/explorer/SearchBarcodeForm.jsx
@@ -1,8 +1,20 @@
+import { faWarning } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { useState } from "react";
+
+function ErrorPopup({text}) {
+    return <div className="p-4 m-2 bg-orange-500 rounded-lg">
+        <p><FontAwesomeIcon icon={faWarning}/>{text}</p>
+    </div>
+}
 
 function SearchBarcodeForm() {
+    const [errorText, setErrorText] = useState("");
+    const [hasError, setHasError] = useState(false);
     let id = 0;
     return (
+       <>
         <form className="search-filters flex flex-row justify-center items-center gap-8 m-4" onSubmit={(e) => {
             e.preventDefault();
             axios.get("/api/read").then((res) => {
@@ -13,7 +25,10 @@ function SearchBarcodeForm() {
                         window.location.href = `/explorer/item/${id}`;
                     }
                 }
-                if (!found) alert(`No item with id ${id} found`);
+                if (!found) {
+                    setErrorText(`No item with id ${id} found`);
+                    setHasError(true);
+                }
             })
         }}>
             <div className="flex flex-col items-center">
@@ -22,6 +37,8 @@ function SearchBarcodeForm() {
             </div>
             <button type="submit" className="p-4 bg-gray-700 rounded-lg text-white transition-all duration-300 hover:bg-sky-600">Search</button>
         </form>
+        {hasError && <ErrorPopup text={errorText}/>}
+        </>
     );
 }
 
