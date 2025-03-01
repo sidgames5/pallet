76135033ede24d994be72c51ddcc79d39f0534fe
SidGames5 <saytleader@outlook.com>
2025-03-01 15:01:16 -0500

    added rounding to tools navbar

--- src/pages/tools/ToolsLayout.jsx
+++ src/pages/tools/ToolsLayout.jsx
@@ -6,11 +6,11 @@ export default function ToolsLayout() {
         ["/tools/barcode", "Barcode Generator"]
     ];
     return (<div className="flex flex-col items-center align-middle justify-center w-full h-full">
-        <div className="topnav flex flex-row my-8 bg-gray-700">
+        <div className="topnav flex flex-row my-8 bg-gray-700 rounded-lg overflow-hidden">
             {links.map((item) => <NavLink
                 key={item[0]}
                 to={item[0]}
-                className={({ isActive }) => `p-4 text-white transition-all duration-300 ${isActive ? "bg-sky-600" : "hover:bg-gray-200 hover:text-black"}`}
+                className={({ isActive }) => `p-2 text-white transition-all duration-300 ${isActive ? "bg-sky-600" : "hover:bg-gray-200 hover:text-black"}`}
             >{item[1]}</NavLink>)}
         </div>
         <Outlet />
