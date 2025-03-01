9244a6d4cf7f01d594fb7a09af7cef60cd85ca57
SidGames5 <saytleader@outlook.com>
2025-03-01 17:24:53 -0500

    got it mostly working

--- src/pages/explorer/BarcodeScanModal.jsx
+++ src/pages/explorer/BarcodeScanModal.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 export default function BarcodeScanModal() {
-    const [data, setData] = useState("Point the camera to a barcode");
     return <div className="bg-gray-300 p-8 rounded-lg w-fit max-w-[40%] fixed flex flex-col justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <p>Scan barcode</p>
         <BarcodeScannerComponent
@@ -13,10 +12,12 @@ export default function BarcodeScanModal() {
                     console.error(err);
                     return;
                 }
-                if (result) setData(result.text);
+                if (result) {
+                    window.location.href = `/explorer/item/${result.text}`;
+                }
             }}
             onError={(err) => console.error(err)}
         />
-        <p>{data}</p>
+        <p>Point the camera to a barcode</p>
     </div>
 }
