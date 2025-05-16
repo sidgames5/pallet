6e52a35efa68921826a9643f3b042ed089c4a865
Sid <saytleader@outlook.com>
2025-05-16 08:58:38 -0400

    added item list in tag info

--- src/pages/tags/TagInfo.jsx
+++ src/pages/tags/TagInfo.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Database from "../../utils/Database";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import LocationsRow from "../explorer/LocationsRow";
 
 export default function TagInfo() {
     const [db, setDb] = useState(null);
@@ -47,9 +49,9 @@ export default function TagInfo() {
         }
     }, [db, effectDbDone, tagId]);
 
-    return <div>
+    return <div className="w-full">
         {tag ? (
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-full gap-4">
                 <div className="flex flex-row items-center gap-4">
                     <FontAwesomeIcon icon={faArrowLeft} className="size-8 p-2 bg-gray-700 rounded-lg text-white cursor-pointer transition-all duration-300 hover:bg-sky-600" onClick={() => navigate(-1)} />
                     <div className={`w-8 h-8 rounded-full bg-${tag.color}-500`}></div>
@@ -59,6 +61,76 @@ export default function TagInfo() {
                         <p>{tag.description}</p>
                     </div>
                 </div>
+
+                <div className="overflow-x-auto w-full">
+                                <table className="search-results w-full text-left rtl:text-right table-auto">
+                                    <thead class="text-white uppercase bg-sky-600">
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Status</th>
+                                            <th>Building</th>
+                                            <th>Area</th>
+                                            <th>Shelf</th>
+                                            <th>Slot</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="*:border-gray-700 *:border-b-2 *:border-l-2 *:border-r-2">
+                                        {db.items.filter((item) => item.tags && item.tags.includes(parseInt(tagId))) 
+                                        .map((item) => <tr>
+                                            <td><Link to={`/explorer/item/${item.id}`}>{item.id}</Link></td>
+                                            <td><Link to={`/explorer/item/${item.id}`}>{item.name}</Link></td>
+                                            <td><select className={`inline-block ${(function (status) {
+                                                switch (status.replace(" ", "").toLocaleLowerCase()) {
+                                                    case "checkedout":
+                                                        return "bg-red-700";
+                                                    case "available":
+                                                        return "bg-green-700";
+                                                    default:
+                                                        return "bg-gray-700";
+                                                }
+                                            })(item.status)} text-white p-1 rounded-lg cursor-pointer`} onChange={(e) => {
+                                                axios.post("/api/edit", {
+                                                    objectId: item.id,
+                                                    category: "items",
+                                                    data: {
+                                                        status: e.currentTarget.value
+                                                    }
+                                                }).then(() => window.location.reload());
+                                            }}>{(function (status) {
+                                                const options = [];
+                                                const defaults = ["available", "checkedout", "unknown"]
+                                                options.push(status.toLocaleLowerCase().replace(" ", ""));
+                                                for (const o of defaults) {
+                                                    if (!options.includes(o)) {
+                                                        options.push(o);
+                                                    }
+                                                }
+                
+                                                const things = [];
+                
+                                                for (const o of options) {
+                                                    switch (o) {
+                                                        case "checkedout":
+                                                            things.push(<option value="checkedout">Checked out</option>);
+                                                            break;
+                                                        case "available":
+                                                            things.push(<option value="available">Available</option>);
+                                                            break;
+                                                        default:
+                                                            things.push(<option value="unknown">Unknown</option>);
+                                                            break;
+                                                    }
+                                                }
+                
+                                                return things;
+                                            })(item.status)}</select></td>
+                                            <LocationsRow item={item} />
+                                            <td>{item.slot}</td>
+                                        </tr>)}
+                                    </tbody>
+                                </table>
+                            </div>
             </div>
         ) : (
             <p>Loading...</p>
